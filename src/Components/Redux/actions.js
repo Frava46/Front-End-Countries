30c7247243e63c34e802e0a395ab4c6ae6802856
Franco V <frava46254425mail.com>
2023-11-09 00:09:23 -0300

    deploy

--- src/Components/Redux/actions.js
+++ src/Components/Redux/actions.js
@@ -6,7 +6,7 @@ import {GET_COUNTRIES, GET_BY_NAME, GET_BY_ID, ORDER, GET_AUX, FILTER, POST_ACTI
 export const getCountries = ()=> {
 
     return async(dispatch) => {
-         const {data} = await axios.get('http://localhost:3001/countries')
+         const {data} = await axios.get('/countries')
 
          // console.log(data)
          
@@ -22,7 +22,7 @@ export const getCountriesByName = (name) => {
 
    return async(dispatch) => {
       try {
-         const {data} = await axios.get(`http://localhost:3001/countries/name?name=${name}`);
+         const {data} = await axios.get(`/countries/name?name=${name}`);
          
          return dispatch({type: GET_BY_NAME, payload:data});
       }catch(error) {
@@ -36,7 +36,7 @@ export const getById = (id) => {
 
    return async(dispatch) => {
       try{
-         const {data} = await axios.get(`http://localhost:3001/countries/${id}`);
+         const {data} = await axios.get(`/countries/${id}`);
          return dispatch({type: GET_BY_ID, payload:data});
       } catch(error) {
          console.log(error.message);
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './Components/Redux/store.js'
+import axios from 'axios'
+
+// axios.defaults.baseURL="http://localhost:3001"
+axios.defaults.baseURL="https://back-end-countries-dev-mhqx.3.us-1.fl0.io"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
